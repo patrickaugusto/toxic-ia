d40aecbdc0c75e6f017bba60c01719b09c026632
patrick_augusto <68711916+patrickaugusto@users.noreply.github.com>
2024-11-13 11:25:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from detoxify import Detoxify
 from fastapi.responses import JSONResponse
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite acesso de qualquer origem; para segurança, pode-se restringir a URLs específicas.
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class TextRequest(BaseModel):
     text: str
